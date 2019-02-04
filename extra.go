9e9017dbcc459c764eb873947f2ad786a876065a
Aleksander Palyan <freeday@apmpc.dp.ua>
2019-02-04 18:20:10 +0200

    Update extra.go

--- extra.go
+++ extra.go
@@ -56,9 +56,9 @@ func (w Whois) Emails() []string {
 }
 
 //Parse uniq name servers from whois
-func (w Whois) ExpiryDate() []string {
+func (w Whois) ExpiryDate() string {
 
-	return parser(regexp.MustCompile(`(?i)Expiry Date:\s+(.*?)(\s|$)`), 1, w.raw)
+	return strings.ToUpper(parser(regexp.MustCompile(`(?i)Expiry Date:\s+(.*?)(\s|$)`), 1, w.raw)[0])
 
 }
 
